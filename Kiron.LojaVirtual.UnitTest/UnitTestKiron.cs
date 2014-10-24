3df766cec2cd39a512515309fff6ff704ea274a7
leandrondias <diasleandro@hotmail.com>
2014-10-24 10:18:05 -0200

    Primeiro

---
+++ Kiron.LojaVirtual.UnitTest/UnitTestKiron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestKiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
